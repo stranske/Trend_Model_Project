4088817952070ab29b4609c9c042b5a12ece25fc
stranske <stranske@gmail.com>
2025-06-25 00:43:57 -0500

    Expose export helpers at package level

--- trend_analysis/__init__.py
+++ trend_analysis/__init__.py
@@ -1,13 +1,28 @@
 """Trend analysis package."""
 
-from . import metrics, config, data, pipeline
+from . import metrics, config, data, pipeline, export
 from .data import load_csv, identify_risk_free_fund
+from .export import (
+    register_formatter_excel,
+    make_summary_formatter,
+    export_to_excel,
+    export_to_csv,
+    export_to_json,
+    export_data,
+)
 
 __all__ = [
     "metrics",
     "config",
     "data",
     "pipeline",
+    "export",
     "load_csv",
     "identify_risk_free_fund",
+    "register_formatter_excel",
+    "make_summary_formatter",
+    "export_to_excel",
+    "export_to_csv",
+    "export_to_json",
+    "export_data",
 ]
