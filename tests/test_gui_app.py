a692cf37acd370fc2e86bd3c9c3734ebe59293ee
stranske <stranske@gmail.com>
2025-07-20 22:23:11 -0500

    Format GUI import test

--- tests/test_gui_app.py
+++ tests/test_gui_app.py
@@ -1,15 +1,19 @@
 import importlib
 import sys
 
+
 def test_gui_module(monkeypatch):
     class Dummy:
         def __init__(self):
             self.titles = []
             self.texts = []
+
         def title(self, txt):
             self.titles.append(txt)
+
         def write(self, txt):
             self.texts.append(txt)
+
     dummy = Dummy()
     monkeypatch.setitem(sys.modules, "streamlit", dummy)
     importlib.import_module("trend_analysis.gui.app")
