07a3f402878386eacf5cdb9791da9933262f1e93
Tim Stranske <stranske@gmail.com>
2026-06-23 14:26:45 -0500

    Tighten Streamlit theme verifier coverage

--- streamlit_app/pages/5_Monte_Carlo.py
+++ streamlit_app/pages/5_Monte_Carlo.py
@@ -3,8 +3,9 @@
 from __future__ import annotations
 
 from streamlit_app.monte_carlo_page import render
-from streamlit_app.theme import apply_ds_theme
+from streamlit_app.theme import apply_density_compact, apply_ds_theme
 
 
 apply_ds_theme()
+apply_density_compact()
 render()
